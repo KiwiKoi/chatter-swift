8ab007f5b36c4e5a402df33ab0873a27d0777dcf
Daniel <danielo.visage@gmail.com>
2023-12-25 15:12:01 +0100

    Initial Commit

---
+++ Chatter/ChatterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatterApp.swift
+//  Chatter
+//
+//  Created by Daniel Visage on 25/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
